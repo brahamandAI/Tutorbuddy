8eddf9f9a20a2e2ee1b100b5da9642101145d024
root <root@ubuntu-cloudpanel.localhost>
2025-07-24 06:27:31 +0000

    wollah changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -128,9 +128,9 @@ export default function Home() {
   return (
     <LearningToolsProvider questionRef="home-page" initialText="">
       <ToolsLauncher />
-      {/* BIG WOLLAH BANNER */}
+      {/* BIG BURREYYY BANNER */}
       <div style={{ width: '100%', background: '#ffb347', padding: '32px 0', textAlign: 'center', fontSize: '3rem', fontWeight: 'bold', color: '#fff', letterSpacing: '2px', borderBottom: '6px solid #ff8300', zIndex: 2000 }}>
-        WOLLAH!!!!!!!!!!!
+        Burrreeeeyyyyy!!!!!!!!!!!!
       </div>
       {/* BIG YELLOW BANNER */}
       <div style={{ width: '100%', background: '#fffbe6', padding: '24px 0', textAlign: 'center', fontSize: '2rem', fontWeight: 'bold', color: '#b8860b', letterSpacing: '1px', borderBottom: '4px solid #ffe066', zIndex: 1000 }}>
